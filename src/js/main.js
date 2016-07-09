6fc0354a431364eb8e810caf128636234f34fd11
Vadik Matveev <fooontic@gmail.com>
2016-07-10 00:03:14 +0300

    made bg animation, remade about block

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/wow/dist/wow.js
+//= ../../bower_components/vivus/dist/vivus.js
 
 //= vendor/jquery.color.js
 //= vendor/jquery.countup.js
